1e5cf2b841752ca236d09155412ff69dfcace215
OhHellNah-no <kirill.domnin1@yandex.ru>
2024-08-16 00:48:17 +0400

    Rework

--- people_making.py
+++ people_making.py
@@ -1,72 +1,42 @@
 from faker import Faker
 import csv
 import random
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
-            "i", "j", "k", "l", "m", "n", "o", "p",
-            "q", "r", "s", "t", "u", "v", "w", "x",
-            "y", "z"]
+import string
+fake = Faker()
+alphabet = string.ascii_lowercase
 domens = ["@yandex.ru", "@mail.ru", "@Gmail.com",
           "@hc.ru", "@r.ru", "@reg.ru", "@sweb.ru"]
-nickname = []
-a = 1
-b = 0
-mail = ""
-fake = Faker()
-is_name = True
-i_name = []
-i_sur_name = []
-phone = ["++7"]
-name = ""
-sur_name = ""
-with open("users_1000000.csv", "a", newline="") as file:
+with open("users_10000.csv", "a", newline="") as file:
     writer = csv.writer(file, delimiter=";")
     writer.writerow(
         ("Name", "Surname", "Phone", "Nickname", "Email")
     )
-while (a < 1000001):
-    person = list(fake.name())
-    while ("." in person):
-        person = list(fake.name())
-    for i in person:
-        if (i != " " and is_name):
-            i_name.append(i)
-        else:
-            if (i == " "):
-                b += 1
-                if (b == 2):
-                    break
-            is_name = False
-            i_sur_name.append(i)
-    i_sur_name.remove(" ")
-    name = "".join(i_name)
-    sur_name = "".join(i_sur_name)
+for _ in range(10000):
+    nickname = []
+    name = ""
+    surname = ""
+    person = fake.name()
+    while ("." in list(person)):
+        person = fake.name()
+    person = person.split(" ")
+    name = person[0]
+    surname = person[1]
+    phone = []
     for _ in range(10):
         phone.append(str(random.randint(0, 9)))
+    phone.insert(0, "+7")
     phone.insert(1, "-")
     phone.insert(5, "-")
     phone.insert(9, "-")
     phone.insert(12, "-")
     phone = "".join(phone)
     for _ in range(9, 19):
-        if (random.randint(0, 1) == 0):
-            nickname.append(alphabet[random.randint(0, len(alphabet)-1)])
-        else:
-            nickname.append(str(random.randint(0, 9)))
+        nickname.append(random.choice(alphabet))
     nickname.insert(random.randint(1, len(nickname)-2), "_")
     nickname = "".join(nickname)
-    mail = nickname + domens[random.randint(0, len(domens)-1)]
-    with open("users_1000000.csv", "a", newline="") as file:
+    mail = nickname + random.choice(domens)
+    with open("users_10000.csv", "a", newline="") as file:
         writer = csv.writer(file, delimiter=";")
         writer.writerow(
-            (name, sur_name, phone, nickname, mail)
+            (name, surname, phone, nickname, mail)
         )
-    nickname = []
-    mail = ""
-    is_name = True
-    i_name = []
-    i_sur_name = []
-    phone = ["++7"]
-    name = ""
-    sur_name = ""
-    b = 0
-    a += 1
