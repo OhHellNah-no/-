387d60b46cb85e616c6de91849b619599ce76430
OhHellNah-no <kirill.domnin1@yandex.ru>
2024-08-22 23:26:49 +0400

    More optimisation

--- people_making.py
+++ people_making.py
@@ -1,35 +1,34 @@
 from faker import Faker
-import csv
-import random
-import string
+import csv import random import string
 fake = Faker()
-users = 0
-USERSGOAL = 10
+USERS_GOAL = 10
 alphabet = string.ascii_lowercase + string.digits
 domens = ["@yandex.ru", "@mail.ru", "@Gmail.com",
           "@hc.ru", "@r.ru", "@reg.ru", "@sweb.ru"]
-with open("users_"+str(USERSGOAL)+".csv", "a", newline="") as file:
+with open(f"users_{USERS_GOAL}.csv", "w", newline="") as file:
     writer = csv.writer(file, delimiter=";")
     writer.writerow(
-        ("Name", "Surname", "Phone", "Nickname", "Email")
+        ("Name", "Surname", "Phone",
+         "Nickname", "Email")
     )
-chose = lambda x: random.choice(x)
-rint = lambda x, y: random.randint(x, y)
-while users < USERSGOAL:
+rchoise = lambda x:
+    random.choice(x)
+rint = lambda x, y:
+    random.randint(x, y)
+for _ in range(USERS_GOAL):
     person = fake.name()
     while ("." in person):
         person = fake.name()
     name, surname = person.split(" ")[:2]
-    nums = ''.join([str(rint(0, 9)) for _ in range(10)])
-    phone = f'+7-{nums[:3]}-{nums[3:6]}-{nums[6:8]}-{nums[8:]}'
+    nums = ''.join([str(rint(0, 9))for _ in range(10)])
+    phone =f"+7-{nums[:3]}-{nums[3:6]}-{nums[6:8]}-{nums[8:]}"
     nickname = ''
-    for _ in range(10):
-        nickname += chose(alphabet)
+    for _ in range(rint(9, 19)):
+        nickname += rchoise(alphabet)
     nickname = f"{nickname[:2]}_{nickname[2:]}"
-    mail = nickname + chose(domens)
-    with open("users_"+str(USERSGOAL)+".csv", "a", newline="") as file:
+    mail = nickname + rchoise(domens)
+    with open(f"users_{USERS_GOAL}.csv", "w", newline="") as file:
         writer = csv.writer(file, delimiter=";")
         writer.writerow(
             (name, surname, phone, nickname, mail)
         )
-    users += 1
