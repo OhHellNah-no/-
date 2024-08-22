456d613c42d8fbabb7269d5cfac41701b5dae63e
OhHellNah-no <kirill.domnin1@yandex.ru>
2024-08-23 01:46:49 +0400

    __init__

--- people_making.py
+++ people_making.py
@@ -18,25 +18,28 @@ with open(f"users_{USERS_GOAL}.csv", "w", newline="") as file:
 
 rchoiсe = lambda x:random.choice(x)
 rint = lambda x, y:random.randint(x, y)
-
-for _ in range(USERS_GOAL):
-    person = fake.name()
-    while ("." in person):
+def generation():
+    for _ in range(USERS_GOAL):
         person = fake.name()
-    name, surname = person.split(" ")[:2]
+        while ("." in person):
+            person = fake.name()
+        name, surname = person.split(" ")[:2]
     
-    nums = ''.join([str(rint(0, 9))for _ in range(10)])
-    phone =f"+7-{nums[:3]}-{nums[3:6]}-{nums[6:8]}-{nums[8:]}"
+        nums = ''.join([str(rint(0, 9))for _ in range(10)])
+        phone =f"+7-{nums[:3]}-{nums[3:6]}-{nums[6:8]}-{nums[8:]}"
 
-    nickname = ''
-    for _ in range(rint(9, 19)):
-        nickname += rchoiсe(alphabet)
-    nickname = f"{nickname[:2]}_{nickname[2:]}"
+        nickname = ''
+        for _ in range(rint(9, 19)):
+            nickname += rchoiсe(alphabet)
+        nickname = f"{nickname[:2]}_{nickname[2:]}"
     
-    mail = nickname + rchoiсe(domens)
+        mail = nickname + rchoiсe(domens)
     
-    with open(f"users_{USERS_GOAL}.csv", "a", newline="") as file:
-        writer = csv.writer(file, delimiter=";")
-        writer.writerow(
-            (name, surname, phone, nickname, mail)
-        )
+        with open(f"users_{USERS_GOAL}.csv", "a", newline="") as file:
+            writer = csv.writer(file, delimiter=";")
+            writer.writerow(
+                (name, surname, phone, nickname, mail)
+            )
+if __name__ == "__main__":
+    generation()
+
