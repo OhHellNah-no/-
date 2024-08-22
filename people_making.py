456babfa72571b69793183dc29505ae89983072d
OhHellNah-no <kirill.domnin1@yandex.ru>
2024-08-22 22:30:57 +0400

    Optimisation

--- people_making.py
+++ people_making.py
@@ -3,40 +3,33 @@ import csv
 import random
 import string
 fake = Faker()
-alphabet = string.ascii_lowercase
+users = 0
+USERSGOAL = 10
+alphabet = string.ascii_lowercase + string.digits
 domens = ["@yandex.ru", "@mail.ru", "@Gmail.com",
           "@hc.ru", "@r.ru", "@reg.ru", "@sweb.ru"]
-with open("users_10000.csv", "a", newline="") as file:
+with open("users_"+str(USERSGOAL)+".csv", "a", newline="") as file:
     writer = csv.writer(file, delimiter=";")
     writer.writerow(
         ("Name", "Surname", "Phone", "Nickname", "Email")
     )
-for _ in range(10000):
-    nickname = []
-    name = ""
-    surname = ""
+chose = lambda x: random.choice(x)
+rint = lambda x, y: random.randint(x, y)
+while users < USERSGOAL:
     person = fake.name()
-    while ("." in list(person)):
+    while ("." in person):
         person = fake.name()
-    person = person.split(" ")
-    name = person[0]
-    surname = person[1]
-    phone = []
+    name, surname = person.split(" ")[:2]
+    nums = ''.join([str(rint(0, 9)) for _ in range(10)])
+    phone = f'+7-{nums[:3]}-{nums[3:6]}-{nums[6:8]}-{nums[8:]}'
+    nickname = ''
     for _ in range(10):
-        phone.append(str(random.randint(0, 9)))
-    phone.insert(0, "+7")
-    phone.insert(1, "-")
-    phone.insert(5, "-")
-    phone.insert(9, "-")
-    phone.insert(12, "-")
-    phone = "".join(phone)
-    for _ in range(9, 19):
-        nickname.append(random.choice(alphabet))
-    nickname.insert(random.randint(1, len(nickname)-2), "_")
-    nickname = "".join(nickname)
-    mail = nickname + random.choice(domens)
-    with open("users_10000.csv", "a", newline="") as file:
+        nickname += chose(alphabet)
+    nickname = f"{nickname[:2]}_{nickname[2:]}"
+    mail = nickname + chose(domens)
+    with open("users_"+str(USERSGOAL)+".csv", "a", newline="") as file:
         writer = csv.writer(file, delimiter=";")
         writer.writerow(
             (name, surname, phone, nickname, mail)
         )
+    users += 1
